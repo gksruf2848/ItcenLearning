cb8413dd577fe7a953f119183ea6b09a433bc569
java <java@102-18>
2023-02-06 09:27:47 +0900

    Fix 10

--- itcen/src/test/Itcen_10_test.java
+++ itcen/src/test/Itcen_10_test.java
@@ -3,7 +3,17 @@ package test;
 public class Itcen_10_test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Test t = new Test();
+		System.out.println(t);
+		t.showInfo();
+		
+		System.out.println(t.name);
+		System.out.println(t.age);
+		Test t2 = t.result();
+		Test t3 = new Test();
+		System.out.println(t);
+		System.out.println(t2);
+		System.out.println(t3);
 
 	}
 
