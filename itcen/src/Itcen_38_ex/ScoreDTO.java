9fe00e73dc0ffe1cd370e1b9492631a999b04b6a
gksruf2848 <4455qqq@naver.com>
2023-02-10 10:13:53 +0900

    38 commit

---
+++ itcen/src/Itcen_38_ex/ScoreDTO.java
@@ -0,0 +1,82 @@
+package Itcen_38_ex;
+
+public class ScoreDTO {
+	private int num;
+	private String name;
+	private int kor;
+	private int eng;
+	private int mat;
+	private int tot;
+	private double avg;
+	
+	public ScoreDTO() {}
+	
+	public ScoreDTO(int num, String name, int kor, int eng, int mat) {
+		this.num = num;
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+		this.tot = kor + eng + mat;
+		this.avg = tot / 3;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+	
+	
+	
+}
